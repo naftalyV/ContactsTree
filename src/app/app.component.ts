4c85519a58c0f61befe13aaf27abc166f4ecdc0e
ויסנשטרן <naftaly276@gmail.com>
2018-06-05 18:36:23 +0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private appService: AppService) { }
+
+  contacts;
+
+  showInner: boolean = false;
+  ngOnInit() {
+    this.contacts = this.appService.getContacts();
+
+    this.contacts.filter(c => c.contacts).map(c => c.show = false);
+
+  }
+
+  showInnerContact(contact) {
+    if (contact.contacts) {
+      contact.contacts.map(c => c.show = !c.show)
+    }
+  }
+  showInInnerContact(contact) {
+    if (contact.contacts) {
+      this.showInner = !this.showInner;
+      contact.contacts.map(c => c.show = this.showInner)
+    }
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor() { }
+
+  getContacts() {
+    var contacts = [
+      {
+        id: 1,
+        name: "Friends",
+        type: "Group",
+        contacts: [
+          { id: 2, name: "Udi", type: "Contact" },
+          { id: 3, name: "Tommy", type: "Contact" },
+          {
+            id: 6,
+            name: "Old Friends",
+            type: "Group",
+            contacts: [
+              { id: 7, name: "Itay", type: "Contact" },
+            ]
+          },
+        ]
+      },
+      {
+        id: 4,
+        name: "Family",
+        type: "Group",
+        contacts: [
+          { id: 5, name: "Roni", type: "Contact" },
+        ]
+      },
+      { id: 8, name: "Ori", type: "Contact" },
+    ];
+
+    return contacts;
+
+  }
+
+}
